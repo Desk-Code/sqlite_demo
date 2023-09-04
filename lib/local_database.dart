6faeb69d3b831819f8b416b510777cc722cfa9a0
Harsh Sangani <harshsangani001@gmail.com>
2023-09-04 19:48:18 +0530

    crud complet in sql

--- lib/local_database.dart
+++ lib/local_database.dart
@@ -1,5 +1,5 @@
-import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:sqlite_demo/user.model.dart';
 
 class LocalDatabaseApi {
@@ -22,6 +22,54 @@ class LocalDatabaseApi {
       _userMst,
       user.toJson(),
       conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  // static Future<List<User>> selectData() async {
+  //   final db = await openDb;
+  //   List<Map<String, dynamic>> data = await db.query(
+  //     _userMst,
+  //   );
+  //   return List.generate(
+  //     data.length,
+  //     (index) => User.fromJson(
+  //       data[index],
+  //     ),
+  //   );
+  // }
+
+  static Future<List<User>> selectData() async {
+    final db = await openDb;
+    final data = await db.rawQuery("select * from $_userMst");
+
+    return data.map((e) => User.fromJson(e)).toList();
+
+    // return List.generate(
+    //   data.length,
+    //   (index) => User.fromJson(
+    //     data[index],
+    //   ),
+    // );
+  }
+
+  static Future<void> updateData(User user) async {
+    final db = await openDb;
+    db.update(
+      _userMst,
+      user.toJson(),
+      where: 'id=?',
+      whereArgs: [user.id],
+    );
+  }
+
+  static Future<void> deleteData(int id) async {
+    final db = await openDb;
+    db.delete(
+      _userMst,
+      where: "id=?",
+      whereArgs: [
+        id,
+      ],
     );
   }
 }
--- lib/user_ui.dart
+++ lib/user_ui.dart
@@ -10,7 +10,18 @@ class UserUIDemo extends StatefulWidget {
 }
 
 class _UserUIDemoState extends State<UserUIDemo> {
+  //
+
   final TextEditingController _textEditingController = TextEditingController();
+  late Future<List<User>> futureUserData;
+  bool isUpdate = false;
+  int? selectedId;
+
+  @override
+  void initState() {
+    futureUserData = LocalDatabaseApi.selectData();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,18 +46,67 @@ class _UserUIDemoState extends State<UserUIDemo> {
               height: 30,
             ),
             ElevatedButton(
-              onPressed: () async {
-                await LocalDatabaseApi.insertData(
-                  User(
-                    userName: _textEditingController.text,
-                  ),
-                );
-                _textEditingController.clear();
-                setState(() {
-
-                });
+              onPressed: isUpdate
+                  ? () async {
+                      User obj = User(
+                        id: selectedId,
+                        userName: _textEditingController.text,
+                      );
+                      await LocalDatabaseApi.updateData(obj);
+                      futureUserData = LocalDatabaseApi.selectData();
+                      _textEditingController.clear();
+                      isUpdate = false;
+                      setState(() {});
+                    }
+                  : () async {
+                      await LocalDatabaseApi.insertData(
+                        User(
+                          userName: _textEditingController.text,
+                        ),
+                      );
+                      futureUserData = LocalDatabaseApi.selectData();
+                      _textEditingController.clear();
+                      isUpdate = false;
+                      setState(() {});
+                    },
+              child: Text(isUpdate ? "Update" : "Submit"),
+            ),
+            FutureBuilder(
+              future: futureUserData,
+              builder: (context, snapshot) {
+                if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) => Dismissible(
+                        key: UniqueKey(),
+                        onDismissed: (direction) {
+                          LocalDatabaseApi.deleteData(
+                            snapshot.data![index].id!,
+                          );
+                          futureUserData = LocalDatabaseApi.selectData();
+                          _textEditingController.clear();
+                          isUpdate = false;
+                          setState(() {});
+                        },
+                        child: ListTile(
+                          title: Text(snapshot.data![index].userName),
+                          trailing: Text("${DateTime.now()}"),
+                          onTap: () {
+                            _textEditingController.text =
+                                snapshot.data![index].userName;
+                            selectedId = snapshot.data![index].id;
+                            isUpdate = true;
+                            setState(() {});
+                          },
+                        ),
+                      ),
+                    ),
+                  );
+                } else {
+                  return const Text("There is no Data...");
+                }
               },
-              child: const Text("Submit"),
             ),
           ],
         ),
